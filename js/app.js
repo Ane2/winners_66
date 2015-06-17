0343d44a8ee05938b2743f0312fb633c0c306baa
LRHJO <louisrhysjones@gmail.com>
2015-06-17 19:39:50 +0100

    Includes blog posts
    Updated front page and included blog posts template

--- js/app.js
+++ js/app.js
@@ -9,8 +9,7 @@ $(document).ready(function() {
       navigation : false, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
-      singleItem:true
-
+      singleItem: true
       // "singleItem:true" is a shortcut for:
       // items : 1,
       // itemsDesktop : false,
